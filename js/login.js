7870ba6162bf2149677c61c268a5f000c361195c
ahasanUllah <88593825+ahasanUllah@users.noreply.github.com>
2022-08-16 15:08:23 +0600

    email changed

--- js/login.js
+++ js/login.js
@@ -1,13 +1,10 @@
 document.getElementById('btn-submit').addEventListener('click', function(){
     const email = document.getElementById('email').value
     const password = document.getElementById('password').value
-    if(email === 'bulbulahasan@gmail.com' && password === 'hellojs'){
+    if(email === 'example@gmail.com' && password === 'hellojs'){
         window.location.href = "bank.html";
     }
     else {
         alert('Please enter valid credential')
     }
-})
-document.getElementById('btn-logout').addEventListener('click', function(){
-    console.log('button clicked');
 })
